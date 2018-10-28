e1e7092edee7066d0e39911280b0b3e4266581f0
satsukiasa <satsukiasa1412@gmail.com>
2018-10-28 17:25:47 +0900

    make

---
+++ KnittingCount/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  KnittingCount
+//
+//  Created by Mika Arai on 2018/10/28.
+//  Copyright © 2018年 Mika Arai. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    class var countGray: UIColor { return #colorLiteral(red: 0.5333333333, green: 0.5333333333, blue: 0.5333333333, alpha: 1) }
+    class var countRed: UIColor { return #colorLiteral(red: 1, green: 0.4, blue: 0.4, alpha: 1) }
+}
--- KnittingCount/ViewController.swift
+++ KnittingCount/ViewController.swift
@@ -8,18 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var totalTextField: UITextField!
+
+    var total: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+//        let numberToolbar = UIToolbar()
+//        numberToolbar.items=[
+//            UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.bordered, target: self, action: "hoopla"),
+//            UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: self, action: nil),
+//            UIBarButtonItem(title: "Apply", style: UIBarButtonItemStyle.bordered, target: self, action: "boopla")
+//        ]
+
+        setup()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    func setup() {
+        countLabel.text = String(0)
+    }
 
+    @IBAction func plusCount(_ sender: Any) {
+        let str = countLabel.text
+        guard let _ = str else { return }
+        var num: Int = Int(str!)!
+
+        num += 1
+        if num == total {
+            countLabel.textColor = UIColor.countRed
+        } else if num > total {
+            num = 1
+            countLabel.textColor = UIColor.countGray
+        }
+        countLabel.text = String(num)
+    }
+
+    @IBAction func minusCount(_ sender: Any) {
+        guard let str = countLabel.text else { return }
+        var num: Int = Int(str)!
+
+        if num == 0 { return }
+        countLabel.textColor = UIColor.countGray
+
+        num -= 1
+        countLabel.text = String(num)
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+
+        guard let str = totalTextField.text else { return }
+        total = Int(str)!
+    }
 }
-
